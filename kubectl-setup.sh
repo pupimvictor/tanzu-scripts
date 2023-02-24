750ef21a8e5d926e5d1fe8976259ef00f66747e6
Victor Pupim <pupimvictor@gmail.com>
2023-02-24 13:54:32 -0300

    add golang install

--- kubectl-setup.sh
+++ kubectl-setup.sh
@@ -25,6 +25,10 @@ if ! type _init_completion; then
 	apt-get install bash-completion 
 fi
 
+# install make 
+sudo apt install make
+
+
 # Set the kubectl completion script source for your shell sessions
 echo 'source <(kubectl completion bash)'>>$HOME/.bashrc
 echo 'source /etc/bash_completion' >>$HOME/.bashrc
@@ -71,17 +75,15 @@ export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(kub
 echo 'source $HOME/git/kube-ps1/kube-ps1.sh' >>$HOME/.bashrc
 echo 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(kube_ps1) λ "' >> $HOME/.bashrc
 
-sudo apt install make
-
-
+# install golang
 wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-
 wget -O $HOME/git/tanzu-login.sh https://gist.githubusercontent.com/pupimvictor/52ae06d56d4a22e0551e414bb93b78d5/raw/c8049ea42d2592f616e68dad6864746256f9c1c9/Tanzu-login.sh
 sudo chmod +x tanzu-login.sh
 source $HOME/git/tanzu-login.sh
+
 
 source $HOME/.bashrc
 
