8b2ddb5c8133cf3cf700f5e8041caa3692bdca01
Victor Pupim <pupimvictor@gmail.com>
2023-09-21 20:12:00 -0300

    prompt: break the script into functions

--- storytime.sh
+++ storytime.sh
@@ -1,52 +1,54 @@
 #!/bin/bash
 
 # Check if TOKEN environment variable is set
-if [ -z "$TOKEN" ]; then
-  echo "Please export the 'TOKEN' environment variable with your Pivotal Tracker API token before running this script."
-  exit 1
-fi
+check_token() {
+  if [ -z "$TOKEN" ]; then
+    echo "Please export the 'TOKEN' environment variable with your Pivotal Tracker API token before running this script."
+    exit 1
+  fi
+}
 
-# Check if PROJECT_ID environment variable is set
-if [ -z "$PROJECT_ID" ]; then
-  read -p "Enter the project ID: " PROJECT_ID
-fi
+# Prompt for the project ID or use the one from the environment
+get_project_id() {
+  if [ -z "$PROJECT_ID" ]; then
+    read -p "Enter the project ID: " PROJECT_ID
+  fi
+}
 
-# Check if PROJECT_ID environment variable is set
-if [ -z "$PROJECT_ID" ]; then
-  read -p "Enter the project ID: " PROJECT_ID
-fi
+# Prompt for user input and validate choices
+prompt_user_input() {
+  read -p "Enter the story name: " STORY_NAME
+  read -p "Enter the story description: " STORY_DESCRIPTION
 
-read -p "Enter the story name: " STORY_NAME
-read -p "Enter the story description: " STORY_DESCRIPTION
+  PS3="Select a story type: "
+  select STORY_TYPE in "feature" "bug" "chore"; do
+    case $STORY_TYPE in
+      "feature"|"bug"|"chore")
+        break
+        ;;
+      *)
+        echo "Invalid choice. Please select a valid story type."
+        ;;
+    esac
+  done
 
-# Prompt for story_type and validate it
-PS3="Select a story type: "
-select STORY_TYPE in "feature" "bug" "chore"; do
-  case $STORY_TYPE in
-    "feature"|"bug"|"chore")
-      break
-      ;;
-    *)
-      echo "Invalid choice. Please select a valid story type."
-      ;;
-  esac
-done
+  PS3="Select a story priority: "
+  select STORY_PRIORITY in "p1" "p2" "p3"; do
+    case $STORY_PRIORITY in
+      "p1"|"p2"|"p3")
+        break
+        ;;
+      *)
+        echo "Invalid choice. Please select a valid story priority."
+        ;;
+    esac
+  done
+}
 
-# Prompt for story_priority and validate it
-PS3="Select a story priority: "
-select STORY_PRIORITY in "p1" "p2" "p3"; do
-  case $STORY_PRIORITY in
-    "p1"|"p2"|"p3")
-      break
-      ;;
-    *)
-      echo "Invalid choice. Please select a valid story priority."
-      ;;
-  esac
-done
-
-# Construct the JSON payload
-JSON_PAYLOAD=$(cat <<-END
+# Create and send the HTTP request, capturing the response code and URL
+send_http_request() {
+  local JSON_PAYLOAD
+  JSON_PAYLOAD=$(cat <<-END
 {
   "kind": "story",
   "story_type": "$STORY_TYPE",
@@ -63,34 +65,31 @@ JSON_PAYLOAD=$(cat <<-END
   ]
 }
 END
-)
+  )
 
-echo $JSON_PAYLOAD | jq
+  local HTTP_RESPONSE
+  HTTP_RESPONSE=$(curl -# -X POST -w "%{http_code}" -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -d "$JSON_PAYLOAD" -o response.json "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories")
 
-# # Send the curl request and capture the response
-# RESPONSE=$(curl -X POST -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories")
+  if [ "$HTTP_RESPONSE" -ge 200 ] && [ "$HTTP_RESPONSE" -lt 300 ]; then
+    local STORY_URL
+    STORY_URL=$(jq -r '.url' response.json)
+    echo "Story created successfully with HTTP code: $HTTP_RESPONSE"
+    echo "Story URL: $STORY_URL"
+  else
+    echo "Error: Failed to create the story. HTTP code: $HTTP_RESPONSE"
+  fi
 
-# # Check if the request was successful
-# if [ $? -eq 0 ]; then
-#   STORY_ID=$(echo "$RESPONSE" | jq -r '.id')
-#   STORY_URL=$(echo "$RESPONSE" | jq -r '.url')
-#   echo "Story created successfully with ID: $STORY_ID"
-#   echo "Story URL: $STORY_URL"
-# else
-#   echo "Error: Failed to create the story."
-# fi
+  # Clean up the response file
+  rm -f response.json
+}
 
-# Send the curl request and capture the HTTP response and response body
-HTTP_RESPONSE=$(curl -X POST -w "%{http_code}" -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -d "$JSON_PAYLOAD" -o response.json "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories")
+# Main script flow
+main() {
+  check_token
+  get_project_id
+  prompt_user_input
+  send_http_request
+}
 
-# Check if the HTTP response code is in the 200 range to determine success
-if [ "$HTTP_RESPONSE" -ge 200 ] && [ "$HTTP_RESPONSE" -lt 300 ]; then
-  STORY_URL=$(jq -r '.url' response.json)
-  echo "Story created successfully with HTTP code: $HTTP_RESPONSE"
-  echo "Story URL: $STORY_URL"
-else
-  echo "Error: Failed to create the story. HTTP code: $HTTP_RESPONSE"
-fi
-
-# Clean up the response file
-rm -f response.json
+# Run the script
+main
