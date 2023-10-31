a39085610f501929ae167c4b5461123c768d1a6c
Victor Pupim <pupimvictor@gmail.com>
2023-10-31 10:14:59 -0300

    remove-shebang

---
+++ tanzu-login2.sh
@@ -0,0 +1,24 @@
+
+if [ -z $TANZU_WCP ]; then
+  echo "export TANZU_WCP with your wcp domain."
+  echo Ex: export TANZU_WCP="https://10.214.180.99"
+  exit 1
+fi
+
+OIFS=$IFS
+IFS="/"
+IN=(${@})
+if [ ! -z ${IN[0]} ] ; then
+  echo Namespace ${IN[0]} 
+  ns="--tanzu-kubernetes-cluster-namespace=${IN[0]}"
+fi
+if [ ! -z ${IN[1]} ] ; then
+  echo ${IN[1]} 
+  cluster="--tanzu-kubernetes-cluster-name=${IN[1]}"
+fi
+
+IFS=$OIFS
+
+cmd="--server $TANZU_WCP --vsphere-username $TANZU_USER --insecure-skip-tls-verify $ns $cluster"
+echo $cmd
+kubectl vsphere login ${cmd}
